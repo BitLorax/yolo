32d59a11d7213ff1c7db2cdab0f8d3a32cad1568
Will Liang <willjhliang@gmail.com>
2022-04-15 10:38:03 -0400

    Kaggle test

--- params.py
+++ params.py
@@ -4,7 +4,7 @@ batch_size = 32
 weight_decay = 0
 momentum = 0.9
 dropout = 0.0
-epochs = 10
+epochs = 1
 
 num_workers = 2
 pin_memory = True
@@ -12,8 +12,8 @@ pin_memory = True
 resume_run = True
 resume_run_id = '2wk8csxx'
 visualize_preds = False
-save_model_file = 'saves/model.pth.tar'
-load_model_file = 'drive/MyDrive/model.pth.tar'
+save_model_file = '../input/yolo-dataset/model.pth.tar'
+load_model_file = '../input/yolo-dataset/04-03-2022_2.pth.tar'
 selected_dataset = 'shape_norot'
 train_data_csv = 'train.csv'
 test_data_csv = 'test.csv'
@@ -28,6 +28,6 @@ elif selected_dataset[0:5] == 'shape':
 
 architecture_size = 'mini'
 
-enable_wandb = True
+enable_wandb = False
 
 config_id = 0

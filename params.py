be166fde8e5704dcd4eca1ce7edc34baa1fb35ed
Will Liang <willjhliang@gmail.com>
2022-04-16 22:21:02 -0400

    Set up run with new architecture

--- params.py
+++ params.py
@@ -4,13 +4,13 @@ batch_size = 32
 weight_decay = 0
 momentum = 0.9
 dropout = 0.0
-epochs = 80 
+epochs = 100 
 
 num_workers = 2
 pin_memory = True
 
-resume_run = True
-resume_run_id = '2wk8csxx'
+resume_run = False
+resume_run_id = ''
 visualize_preds = False
 save_model_file = 'saves/model.pth.tar'
 load_model_file = '../input/yolo-checkpoints/04-03-2022_2.pth.tar'
@@ -26,7 +26,7 @@ if selected_dataset == 'voc':
 elif selected_dataset[0:5] == 'shape':
     C = 5
 
-architecture_size = 'mini'
+architecture_size = 'semi-mini'
 
 enable_wandb = True
 
